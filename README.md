555edfd13c5052a4c6ec5c26b58852ec6e62cec4
wujidadi <wujidadi@gmail.com>
2020-09-22 10:18:30 +0800

    初始化 Git 專案

---
+++ README.md
@@ -0,0 +1,5 @@
+# Beacon Notification Test
+
+Flutter 藍牙與通知結合測試專案。
+
+基於 [**Beacons Plugin Test** 專案](https://github.com/Wujidadi/Flutter-Beacons-Plugin-Test)。
---
+++ android/app/src/main/kotlin/com/example/beacon_notification_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.beacon_notification_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
